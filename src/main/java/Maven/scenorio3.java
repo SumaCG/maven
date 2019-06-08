279b68972e863a266a12745140518e3b6caf6d0d
suma <nsuma29777@gmail.com>
2019-06-08 10:01:50 +0530

    commit

---
+++ src/main/java/Maven/scenorio3.java
@@ -0,0 +1,35 @@
+package Maven;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class scenorio3 {
+	@Test
+    public void test13() throws InterruptedException
+    {
+	    WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.bluestone.com/");
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		WebElement ele = driver.findElement(By.xpath("//li[@class='menuparent offers-menuparent pull-right']/a"));
+		Actions a=new Actions(driver);
+		a.moveToElement(ele).perform();
+		WebElement ele5 = driver.findElement(By.xpath("//div[@class='wh-submenu single-column-submenu width-auto']/descendant::span[@class='prcs-d'][3]"));
+		ele5.click();
+		WebElement ele5V = driver.findElement(By.xpath("//div[@class='filtered-data']/descendant::span[@class='filterDisplayName']"));
+		System.out.println(ele5V.getText());
+		Assert.assertEquals(ele5V.getText(),"Flat 15% Off");
+		System.out.println("Products displayed with Flat 15% Off");	
+		driver.close();
+    }
+}
+
